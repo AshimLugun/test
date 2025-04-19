db37680baf3e1c99ec160272933d6b490175a41e
ashim <ashimhiras@gmail.com>
2025-04-19 10:33:20 +0530

    msg

---
+++ src/main/java/com/test/TestApplication.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=90;
+		String str="abc";
+		boolean a=true;
+		long l=90;
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
